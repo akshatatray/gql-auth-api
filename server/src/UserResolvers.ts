b431a6086932ea5a07f16fe1ca35d45b51a26200
Akshat Atray <akshatatray31@gmail.com>
2021-08-21 13:02:19 +0530

    Register Mutation & Fetch Users Query 🗺

--- server/src/UserResolvers.ts
+++ server/src/UserResolvers.ts
@@ -1,9 +1,41 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { User } from "./entity/User";
+import { hash } from "bcryptjs";
 
 @Resolver()
 export class UserResolver {
     @Query(() => String)
     hello() {
         return "Hello!";
+    }
+    
+    @Query(() => [User])
+    users() {
+        return User.find();
+    }
+
+    @Mutation(() => Boolean)
+    async register(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+    ) {
+        const hashedPass = await hash(password, 12);
+        try {
+            const isAvailable = await User.findOne({
+                where: {email}
+            });
+            if (!isAvailable) {
+                await User.insert({
+                    email,
+                    password: hashedPass,
+                });
+            } else {
+                throw new Error("Email already exists!");
+            }
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+        return true;
     }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,13 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./userResolvers";
+import { createConnection } from "typeorm";
 
 (async () => {
     const app = express();
     app.get("/", (_req, res) => res.send("Hello World!"));
+
+    await createConnection();
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
