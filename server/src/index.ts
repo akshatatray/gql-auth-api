9f86cb2ced394912f6cafba77f01ebfb1b8bb722
Akshat Atray <akshatatray31@gmail.com>
2021-08-21 11:01:42 +0530

    GraphQL Resolvers 🔨

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./userResolvers";
 
 (async () => {
     const app = express();
     app.get("/", (_req, res) => res.send("Hello World!"));
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver]
+        })
+    });
+
+    await apolloServer.start()
+
+    apolloServer.applyMiddleware({ app });
+
     app.listen(4000, () => {
         console.log("App is running on port 4000");
     });
